1f5e8ba4788ac293e564a4fb0eb62bb6b179ef62
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_trellism4.py
+++ adafruit_trellism4.py
@@ -30,7 +30,7 @@ import digitalio
 import neopixel
 import adafruit_matrixkeypad
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_TrellisM4.git"
 
 
